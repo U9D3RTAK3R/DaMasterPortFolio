272774cc99354b3e7d4a8a6f9d175ecbac9bd82d
U9D3RTAK3R <104409038+U9D3RTAK3R@users.noreply.github.com>
2025-07-02 20:00:02 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import { Navbar } from "../components/NavBar";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+import { HeroSection } from "@/components/HeroSection";
+import { AboutSection } from "@/components/AboutSection";
+import { SkillsSection } from "@/components/SkillsSection";
+import { ContactSection } from "@/components/ContactSection";
+import { Footer } from "@/components/Footer";
+
+export const Home = () => {
+    return(
+        <div className="min-h-screen">
+            {/* Theme Toggle */}
+            <ThemeToggle />
+            {/* Background Effects */}
+            <StarBackground />
+            {/* Navbar */}
+            <Navbar />
+            {/* Main Content */}
+            <main>
+                <HeroSection />
+                <AboutSection />
+                <SkillsSection />
+                <ContactSection />
+            </main>
+            {/* Footer */}
+            <Footer />
+        </div>
+    );
+}
